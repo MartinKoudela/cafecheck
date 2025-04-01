0088c45e3f9117d0ae12ee772328670457b61a38
swa666rrass <koudela.m@icloud.com>
2025-04-01 23:31:20 +0200

    javascript email sender & email responder - done, to do - the remaining html

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-function sendMail(){
+function getMail(){
     let parms = {
         name : document.getElementById("name").value,
         email : document.getElementById("email").value,
         message : document.getElementById("message").value,
     }
 
-    emailjs.send("service_l1shzrp", "template_pzokilg",parms).then(alert("Email byl zaslán!"))
-}
+    emailjs.send("service_l1shzrp", "template_pzokilg",parms)
+    emailjs.send("service_l1shzrp", "template_cufyelu",parms)
 
+
+        .then(() => {
+            alert("Děkujeme za zpětnou vazbu, byl vám zaslán email!");                // alert za vyplneni
+            document.getElementById("feedbackForm").reset();    // reset formulare
+        })
+        .catch((error) => {
+            console.error("Chyba při ukládání:", error);        // chyba alert
+            alert("Nastala chyba při odesílání zpětné vazby.");
+        });
+}
